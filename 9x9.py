04e3ca05093c5d58a8eaaf05dae70bf4e5f32fdb
EmeraldLE <alst1997@gmail.com>
2020-05-26 13:25:13 +0800

    lesson 1 of python - 9x9 (2)

--- 9x9.py
+++ 9x9.py
@@ -1,8 +1,8 @@
+
 for i in [1,2,3,4,5,6,7,8,9]:
     for j in [1,2,3,4,5,6,7,8,9]:
-        str(i)
-        str(j)
-        print(str(i)+"x"+str(j)+"="+str(i*j))
-
+        print(str(i)+"x"+str(j)+"="+str(i*j)+" ",end="")
+    print(" ")
+print("\n")
 for a in [1,2,3,4,5,6,7,8,9]:
     print("1x"+str(a)+"="+str(1*a)+" 2x"+str(a)+"="+str(2*a)+" 3x"+str(a)+"="+str(3*a)+" 4x"+str(a)+"="+str(4*a)+" 5x"+str(a)+"="+str(5*a)+" 6x"+str(a)+"="+str(6*a)+" 7x"+str(a)+"="+str(7*a)+" 8x"+str(a)+"="+str(8*a)+" 9x"+str(a)+"="+str(9*a))
